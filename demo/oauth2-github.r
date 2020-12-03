ba51b05d999387fcfe6f6d05e0f5919a95d90df6
Jellyfish1223 <69008277+Jellyfish1223@users.noreply.github.com>
2020-12-03 12:19:00 +0000

    Web Scraping a GitHub Repo

--- demo/oauth2-github.r
+++ demo/oauth2-github.r
@@ -10,8 +10,8 @@ oauth_endpoints("github")
 #
 #    Replace your key and secret below.
 myapp <- oauth_app("github",
-  key = "56b637a5baffac62cad9",
-  secret = "8e107541ae1791259e9987d544ca568633da2ebf"
+  key = "d52ecea8796fe5791f29",
+  secret = "216c05a111095b0d8e853af2547f8add3e48dd14"
 )
 
 # 3. Get OAuth credentials
@@ -19,11 +19,11 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # 4. Use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/rate_limit", gtoken)
+req <- GET("https://api.github.com/repos/jtleek/datasharing", gtoken)
 stop_for_status(req)
-content(req)
+content(req)$created_at
 
-# OR:
-req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
-stop_for_status(req)
-content(req)
+# # OR:
+# req <- with_config(gtoken, GET("https://api.github.com/users/jtleek/repos"))
+# stop_for_status(req)
+# content(req)
